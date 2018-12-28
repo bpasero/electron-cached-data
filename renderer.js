4ce75b765514e913fe4da82bd5ccc518d65ee99b
Benjamin Pasero <benjpas@microsoft.com>
2018-12-28 12:04:18 +0100

    tweaks

--- renderer.js
+++ renderer.js
@@ -4,7 +4,7 @@
 const vm = require("vm");
 const fs = require("fs");
 
-const CACHE_FILE = 'cache.code';
+const CACHE_FILE = `cache.${process.versions.electron}.code`;
 
 let options;
 if (fs.existsSync(CACHE_FILE)) {
@@ -13,21 +13,31 @@ if (fs.existsSync(CACHE_FILE)) {
     options = { produceCachedData: true, displayErrors: true };
 }
 
-global.define = () => {};
+global.define = () => { };
 
-const now = Date.now();
-const script = new vm.Script(fs.readFileSync('workbench.main.js').toString('utf8'), options);
-script.runInThisContext(options);
-console.log((Date.now() - now) + "ms");
+// Run once to ensure cached data
+runScript();
+options = { cachedData: fs.readFileSync(CACHE_FILE) };
 
-if (script.cachedDataProduced) {
-    fs.writeFileSync(CACHE_FILE, script.cachedData);
-    console.log("Did produce cached data");
-} else {
-    if (script.cachedDataRejected) {
-        console.log("Did not produce cached data. IT WAS REJECTED!");
-    } else {
-        console.log("Did not produce cached data. Loaded from cache!");
-    }
+// Measure N times to reduce spikes
+let totalMeasurements = 500;
+let totalDuration = 0;
+for (let i = 0; i < totalMeasurements; i++) {
+    totalDuration += runScript();
 }
 
+console.log((totalDuration / totalMeasurements) + 'ms');
+
+function runScript() {
+    const now = Date.now();
+    const script = new vm.Script(fs.readFileSync('workbench.main.js').toString('utf8'), options);
+    script.runInThisContext(options);
+    const duration = Date.now() - now;
+
+    if (script.cachedDataProduced) {
+        fs.writeFileSync(CACHE_FILE, script.cachedData);
+        console.log("Did produce cached data");
+    }
+
+    return duration;
+}
